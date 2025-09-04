333fe84e07487bb1c23d59ff54e35656b5b7c4ff
Ketan Kunkalikar <ketankunkalikar33@gmail.com>
2025-09-04 14:46:29 +0530

    config has prod backend url for prod and dev

--- wallpaper_app/constants/Config.ts
+++ wallpaper_app/constants/Config.ts
@@ -5,7 +5,7 @@
 export const Config = {
   api: {
     baseUrl: __DEV__ 
-      ? 'http://192.168.101.79:8000'  // Development - Changed port to 8000
+      ? 'https://wallpaperapp-production.up.railway.app'  // Development - Changed port to 8000
       : 'https://wallpaperapp-production.up.railway.app',  // Production
   },
 } as const;
